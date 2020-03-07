42de715ce8ab18eb309a6df9e380cc3f2aa1af9a
bradenn <braden@bradencnicholson.com>
2020-03-06 20:32:12 -0800

    NODE env port

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ let express = require('express');
 let index = express();
 
 index.use(express.json());
-index.use(express.urlencoded({ extended: false }));
+index.use(express.urlencoded({extended: false}));
 let apiRoute = require('./routes');
 
 index.use('/', apiRoute);
 
+
 index.listen(5001, function () {
-    console.log('Express server started. Listing on port 5001.');
+    let port = 5001;
+    if (process.env.PORT) port = process.env.PORT;
+    console.log(`Express server started. Listing on port ${port}.`);
 });
 
 module.exports = index;
